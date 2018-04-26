ae5faf376e9b9242ea1c687d83d0d0b5ae11242d
nicovank <nvankemp@gmail.com>
2018-04-26 12:00:25 -0400

    Modal fields are now editable.

--- staff/src/Employee/Modal.js
+++ staff/src/Employee/Modal.js
@@ -25,6 +25,14 @@ class EmployeeModal extends Component {
 		}
     }
 
+    handleChange(field) {
+		return event => {
+			const state = {};
+			state[field] = event.target.value;
+			this.setState(state);
+		}
+	}
+
 	render() {
 		return (
 			<div id="employee-modal" className="modal-box hide">
@@ -34,27 +42,27 @@ class EmployeeModal extends Component {
 					<div className="modal-body">
 						<form className="form">
 							<div className="form-item">
-								<label>Student ID <input type="text" value={this.state.id}></input></label>
+								<label>Student ID <input type="text" onChange={this.handleChange("id").bind(this)} value={this.state.id}></input></label>
 							</div>
 							<div className="form-item">
-								<label>First Name <input type="text" value={this.state.firstName}></input></label>
+								<label>First Name <input type="text" onChange={this.handleChange("firstName").bind(this)} value={this.state.firstName}></input></label>
 							</div>
 							<div className="form-item">
-								<label>Last Name <input type="text" value={this.state.lastName}></input></label>
+								<label>Last Name <input type="text" onChange={this.handleChange("lastName").bind(this)} value={this.state.lastName}></input></label>
 							</div>
 							<div className="form-item">
 								<label>Position
-									<select>
+									<select value={this.state.position} onChange={this.handleChange("position").bind(this)}>
 										<option value="DA">Desk Attendant</option>
 										<option value="RA">Resident Assistant</option>
 									</select>
 								</label>
 							</div>
 							<div className="form-item">
-								<label>Email <input type="text" value={this.state.email}></input></label>
+								<label>Email <input type="text" onChange={this.handleChange("email").bind(this)} value={this.state.email}></input></label>
 							</div>
 							<div className="form-item">
-								<label>Phone Number <input type="text" value={this.state.phoneNb}></input></label>
+								<label>Phone Number <input type="text" onChange={this.handleChange("phoneNb").bind(this)} value={this.state.phoneNb}></input></label>
 							</div>
 							<button>Save</button>
 						</form>
